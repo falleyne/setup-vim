2cecad7c9d8d3dab9be8c5a19ae3bbe23a56cd22
Farai Alleyne <falleyne@glgroup.com>
2012-12-13 11:01:26 -0500

    first commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#setup pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+curl -Sso ~/.vimrc \
+	https://raw.github.com/falleyne/setup-vim/.vimrc
+
